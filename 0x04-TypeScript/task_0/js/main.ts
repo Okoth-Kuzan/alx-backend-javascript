b8f80abcf6f2fed58cca0f4afd11c47aca3f319c
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2024-12-05 15:00:31 +0000

    0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,70 +1,47 @@
-
-/**
- * Write an interface named Studentthat accepts the
- * following elements:
- * - firstName(string),
- * - lastName(string),
- * - age(number),
- * - location(string)
- */
-
 export interface Student {
-  firstName: string,
-  lastName: string,
-  age: number,
-  location: string,
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
 }
-
-/**
- * Create two students, and create an array named studentsList
- * containing the two variables
- */
-
 const student1: Student = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 25,
+  firstName: 'First',
+  lastName: 'Student',
+  age: 20,
+  location: 'Nairobi',
+};
+const student2: Student = {
+  firstName: 'Second',
+  lastName: 'Student',
+  age: 100,
   location: 'Nairobi',
 };
 
-const student2: Student = {
-  firstName: 'Alex',
-  lastName: 'Sanchez',
-  age: 29,
-  location: 'Cairo',
-};
+const studentsList: Array<Student> = [student1, student2];
 
-const studentsList: Array<Student> = [
-  student1,
-  student2,
-];
-
-
-/**
- * - Using Vanilla Javascript, render a table and for each elements in the array,
- * append a new row to the table
- * 
- * - Each row should contain the first name of the student and the location
- */
-
-export const renderTable = (studentsList: Array<Student>): void =>  {
-  // create table tag
-  const table = document.createElement('table');
-  const headRow = document.createElement('tr');
-  table.insertAdjacentElement('beforeend', headRow);
-
-  // insert headers
-  headRow.insertAdjacentHTML('beforeend', '<th>FirstName</th>');
-  headRow.insertAdjacentHTML('beforeend', '<th>Location</th>');
-
-  for (const student of studentsList) {
-    const studentRow = document.createElement('tr')
-    studentRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
-    studentRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
-    table.insertAdjacentElement('beforeend', studentRow);
-  }
-
-  document.body.insertAdjacentElement('beforeend', table);
+function render(students: Array<Student>) {
+  const table = document.createElement('TABLE');
+  const thead = document.createElement('THEAD');
+  const thead_tr = document.createElement('TR');
+  const tbody = document.createElement('TBODY');
+  Object.keys(students[0]).forEach((k) => {
+    const th = document.createElement('TH');
+    th.innerText = k;
+    thead_tr.appendChild(th);
+  });
+  thead.appendChild(thead_tr);
+  table.appendChild(thead);
+  students.forEach((student: Student) => {
+    const tr = document.createElement('TR');
+    Object.values(student).forEach((v) => {
+      const td = document.createElement('TD');
+      td.innerText = v.toString();
+      tr.appendChild(td);
+    });
+    tbody.appendChild(tr);
+  });
+  table.appendChild(tbody);
+  document.querySelector('body').appendChild(table);
 }
 
-renderTable(studentsList);
+render(studentsList);
